d5801d2b6e227afdb316cb4c99fa8718ba7f10b0
Paolagbr <paolagbr555@gmail.com>
2025-12-06 13:09:13 -0600

    Ipcorregida

--- src/app/services/audio-mixer.service.ts
+++ src/app/services/audio-mixer.service.ts
@@ -137,8 +137,8 @@ export class AudioMixerService {
       await this.audioContext.resume();
     }
 
-    const UBUNTU_IP = '192.168.50.69';
-    const PORT = '3000';
+    const UBUNTU_IP = '192.168.193.146';
+    const PORT = '8888';
 
     if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
       this.socket = new WebSocket(`ws://${UBUNTU_IP}:${PORT}`);
